9c491e0bf9e699559138fa28f97fd659db33a4f3
mohitsirohi1990 <mohitsirohi99@gmail.com>
2017-12-26 17:00:49 +0530

    about

---
+++ about.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/delay';
+
+import {Leader} from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { flyInOut } from '../animations/app.animation';
+import { expand } from '../animations/app.animation';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
+})
+export class AboutComponent implements OnInit {
+leader: Leader[];
+
+  constructor(private leaderservice: LeaderService,
+    private route: ActivatedRoute,
+    private location: Location) { }
+
+  ngOnInit() {
+       this.leaderservice.getLeaders()
+       .subscribe(leader => this.leader = leader);
+  }
+
+}
