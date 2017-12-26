25e2b66ec85e759ad924be0ad3c481f78ea73942
mohitsirohi1990 <mohitsirohi99@gmail.com>
2017-12-26 17:05:31 +0530

    Home folder files

---
+++ home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { LeaderService } from '../services/leader.service';
+import { Promotion } from '../shared/promotion';
+import { Leader } from '../shared/leader';
+import { PromotionService } from '../services/promotion.service';
+
+import { flyInOut } from '../animations/app.animation';
+import { expand } from '../animations/app.animation';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  dish: Dish;
+  promotion: Promotion;
+  leader: Leader;
+
+  dishErrMess : string;
+
+  constructor(private dishservice: DishService,
+    private promotionservice: PromotionService,private leaderservice: LeaderService,@Inject('BaseURL') private BaseURL) { }
+
+  ngOnInit() {
+    this.dishservice.getFeaturedDish().subscribe(dish => this.dish =dish,dishErrMess => this.dishErrMess = <any>dishErrMess);
+    this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader = leader,dishErrMess => this.dishErrMess = <any>dishErrMess);
+    this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion,dishErrMess => this.dishErrMess = <any>dishErrMess);
+  }
+
+}
